0cbc6ef2456f6fd8da9d1c6ebb7b57831de9b35b
microup <ofp1979@yandex.ru>
2023-10-28 09:52:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RealBuyParts
+
+This mod is created to prevent the possibility of buying body parts for cars older than 2000 in the store. To obtain the necessary parts for the car you are working on, you will have to search for all the body elements in a junkyard, barns, or auctions. This mod makes the game a little more challenging and interesting. In future versions of the mod, various engines and parts for old cars will also be removed from the store.
+
+
+## How install: 
+1. Download realshop.dll
+2. Download and install [MelonLoader](https://melonwiki.xyz/#/?id=requirements) ----> **!!!!   Only 0.5.7 VERSION**
+3. Copy the dll file to the **"Car Mechanic Simulator 2021\Mods"**
---
+++ RealShop/RealShop.cs
@@ -0,0 +1,43 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace RealShop
+{
+    public class RealParts : MelonMod
+    {
+        [Obsolete]
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("initializing...");
+        }
+
+        public override void OnUpdate()
+        {
+
+            GameObject shopCarWindow = GameObject.Find("ShopCarWindow");
+            if (shopCarWindow != null)
+            {
+                var itemsCars = shopCarWindow.transform.Find("Items");
+                if (itemsCars != null)
+                {
+                    for (int i = 0; i < itemsCars.childCount; i++)
+                    {
+                        Transform ShowroomCarItem21Shop = itemsCars.GetChild(i);
+                        GameObject ShowroomCarObj = ShowroomCarItem21Shop.gameObject;
+                        var carName = ShowroomCarItem21Shop.Find("CarName").GetComponent<Text>();
+                        var carYear = Helpers.GetYearFromString(carName.text);
+                        if (carYear < 2000)
+                        {
+                            ShowroomCarObj.SetActive(false);
+                            MelonLogger.Msg($"Car year {carYear}");
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
