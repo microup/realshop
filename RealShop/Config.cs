26726dcf938c9df6dbfdca4d6a7d677aae867159
microup <ofp1979@yandex.ru>
2023-11-04 19:40:35 +0300

    ver_0.2

--- RealShop/Config.cs
+++ RealShop/Config.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace RealShop
 {
@@ -11,18 +12,56 @@ namespace RealShop
     {
         public const string ModName = "RealShop";
 
+        public static string[] Brands = { "trando", "voiz", "fierte", "shushutri", "bardogh" };
+
+        public static string[] TargetRepairParts = { "mirror", "window", "seat", "taillight", "pasek", "lancuch",
+            "srodkowy", "katalizator", "koncowy", "rura", "slizg", "filtr", "inverter", "wheel", "turbo", "kable", "challenger",
+            "washer", "reservoir", "srodkowy", "bak", "rolka", "walka", "pasek", "poczatkowy", "charging", "cewka", "tlumik", "wzdluzny"};
+        public static int[] RepairCost = {150, 250, 400};
+
         private const string ConfigCategoryName = "MainSection";
+        private const string ConfigRepairSection = "RepairSection";
 
         private readonly MelonPreferences_Category _config;
+        private readonly MelonPreferences_Category _repair;
+        private readonly MelonPreferences_Entry<KeyCode> _isEnableSwitchModeBindKey;
+        private readonly MelonPreferences_Entry<KeyCode> _printStackBindKey;
+        private readonly MelonPreferences_Entry<KeyCode> _repairBindKey;
+        private readonly MelonPreferences_Entry<bool> _isShowDebugInfo;
         private readonly MelonPreferences_Entry<int> _removeCarsOlderThanYear;
+        private readonly MelonPreferences_Entry<bool> _isEnableBrandFilter;
+        private readonly MelonPreferences_Entry<string[]> _brandsEntry;
+        private readonly MelonPreferences_Entry<string[]> _repairParts;
+        private readonly MelonPreferences_Entry<int[]> _repairCost;
 
+        public KeyCode IsEnableSwitchMode => _isEnableSwitchModeBindKey.Value;
+        public KeyCode PrintStack => _printStackBindKey.Value;
+        public KeyCode Repair => _repairBindKey.Value;
+        public bool IsShowDebugInfo => _isShowDebugInfo.Value;
         public int RemoveCarsOlderThanYear => _removeCarsOlderThanYear.Value;
+        public string[] PartBrands => _brandsEntry.Value;
+        public string[] RepairParts => _repairParts.Value;
+        public int[] ListRepairCost => _repairCost.Value;
+        public bool IsEnableBrandFilter => _isEnableBrandFilter.Value;
+
         public Config()
         {
             _config = MelonPreferences.CreateCategory(ConfigCategoryName);
             _config.SetFilePath("Mods/RealShop.cfg");
 
+            _isEnableSwitchModeBindKey = _config.CreateEntry(nameof(IsEnableSwitchMode), KeyCode.F9);
+
+            _isShowDebugInfo = _config.CreateEntry(nameof(IsShowDebugInfo), false);
             _removeCarsOlderThanYear = _config.CreateEntry(nameof(RemoveCarsOlderThanYear), 2000);
+            _isEnableBrandFilter = _config.CreateEntry(nameof(IsEnableBrandFilter), true);
+            _brandsEntry = _config.CreateEntry(nameof(PartBrands), Brands);
+
+            _repair = MelonPreferences.CreateCategory(ConfigRepairSection);
+            _repair.SetFilePath("Mods/RealShop.cfg");
+
+            _repairBindKey = _repair.CreateEntry(nameof(Repair), KeyCode.F8);
+            _repairParts = _repair.CreateEntry(nameof(RepairParts), TargetRepairParts);
+            _repairCost = _repair.CreateEntry(nameof(ListRepairCost), RepairCost);
         }
 
         public void Reload()
--- RealShop/Helpers.cs
+++ RealShop/Helpers.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace RealShop
 {
@@ -17,6 +19,50 @@ namespace RealShop
             {
                 return -1;
             }
+        }
+
+        public static bool IsBrandExist(string brand)
+        {
+            foreach (string name in Config.Brands)
+            {
+                if (brand.Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CheckForSubstrings(string input, string[] targets)
+        {
+            return targets.Any(target => input.Contains(target));
+        }
+
+        public static string ToPrettyString(Item item)
+        {
+            if (item == null) return "";
+
+            return ($"(Item) ID: {item.ID}, UID: {item.UID}, Condition: {item.Condition}, Dent: {item.Dent}, " +
+                    $"Quality: {item.Quality}, IsExamined: {item.IsExamined}, RepairAmount: {item.RepairAmount}, " +
+                    $"WashFactor: {item.WashFactor}, OutsideRustEnabled: {item.OutsideRustEnabled}, Livery: {item.Livery}, " +
+                    $"LiveryStrength: {item.LiveryStrength}, NormalID: {item.NormalID}, ConditionToShow: {item.ConditionToShow}," +
+                    $"LPData: {item.LPData}");
+        }
+
+        public static string ToPrettyString(GroupItem groupItem)
+        {
+            if (groupItem == null) return "null";
+
+            return ($"(GroupItem) ID: {groupItem.ID}, UID: {groupItem.UID}, " +
+                            $"Condition: {groupItem.GetCondition()}, ConditionToShow: {groupItem.GetConditionToShow()}, " +
+                            $"Size: {groupItem.Size}, IsNormalGroup: {groupItem.IsNormalGroup}");
+        }
+
+        public static string ToPrettyString(BaseItem baseItem)
+        {
+            if (baseItem == null) return "null";
+            return ($"(BaseItem) ID: {baseItem.ID}, UID: {baseItem.UID}, " +
+                            $"Condition: {baseItem.GetCondition()}, ConditionToShow: {baseItem.GetConditionToShow()}");
         }
     }
 }
