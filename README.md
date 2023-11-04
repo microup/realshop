85cf9102e08c30155301da9d9cdfae6fba6482f9
microup <ofp1979@yandex.ru>
2023-11-04 19:40:35 +0300

    ver_0.2

--- README.md
+++ README.md
@@ -1,17 +1,33 @@
-# RealBuyParts
+# RealShop
 
 This mod is created to prevent the possibility of buying body parts for cars older than 2000 in the store. To obtain the necessary parts for the car you are working on, you will have to search for all the body elements in a junkyard, barns, or auctions. This mod makes the game a little more challenging and interesting. Also I hope that it will slightly change the gaming experience, making it a bit more challenging and engaging. I hope you will like it.
 
 ## Features include:
 
-You will need to search for the necessary parts for old cars and restore them; it is no longer possible to buy new parts for retro or old cars.
+**IMPORTANT AND RECOMMENDED! This mod should be used only when you have unlocked the entire branch for repairing parts and have purchased the necessary machines for repairing body and other parts.
 
-## How install: 
+Features include:
+
+- You will need to search for the necessary parts for old cars and restore them; it is no longer possible to buy new parts for retro or old cars.
+- Now junkyards, barns, and auctions take on a different meaning.
+- Now all body and interior parts can be repaired.
+- Now you can repair generic parts from all non-default manufacturers.
+- Added a configuration file where you can:
+           - Change the year for cars that will not appear in the shop.
+           - Change the binding key to enable or disable the mod.
+           - Add or remove specific brands from the list that won't appear in the shop.
+
+
+Important! Many parts in the shop will be unavailable. Now, junkyard visits have become a necessity. Non-default items won't be found in the shop, but you can find them in the barn, at auctions, or in the junkyard, and then repair them.
+
+Realism is getting closer, enjoy! :-)
+
+## How install:
 1. Download realshop.dll
 2. Download and install [MelonLoader](https://melonwiki.xyz/#/?id=requirements) ----> **!!!!   Only 0.5.7 VERSION**
 3. Copy the dll file to the **"Car Mechanic Simulator 2021\Mods"**
 
-## 
+##
 In the plans, I will remove engines, tires, and other items for old and retro cars from the store. Now junkyards, barns, and auctions take on a different meaning.
 
 ## Before
--- RealShop/RealShop.cs
+++ RealShop/RealShop.cs
@@ -1,48 +1,313 @@
 ﻿using MelonLoader;
+using Harmony;
 using UnityEngine;
 using UnityEngine.UI;
+using OrbCreationExtensions;
+using System.Collections.Generic;
+using System.Linq;
+using CMS.UI.Windows;
+using CMS.UI.Logic;
 using System;
+using CMS.Difficulty;
 
+
+// TODO:
+// сделать что бы все предметы могли ремонтироваться
+// сделать что с первого уровня доступны все станки для ремонта
+// посещение свалки с перого уровня и бесплатно
 namespace RealShop
 {
     public class RealParts : MelonMod
     {
-
         private Config _config;
+        private bool _modIsEnable = true;
+        private bool _runOnes = false;
+
+        GameObject _blocker_Repair_Parts_2;
+        GameObject _blocker_Repair_Parts_3;
 
         [Obsolete]
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("initializing...");
+
             _config = new Config();
             _config.Reload();
         }
 
         public override void OnUpdate()
         {
+            if  (Input.GetKeyDown(_config.IsEnableSwitchMode))
+            {
+                if (_modIsEnable)
+                {
+                    _modIsEnable = false;
+                    UIManager.Get().ShowPopup("[RealShop]","has been stoped.", PopupType.GameMode);
+                } else {
+                    _modIsEnable = true;
+                    UIManager.Get().ShowPopup("[RealShop]", "has been enabled.", PopupType.GameMode);
+                }
+            }
+
+            if (Input.GetKeyDown(_config.Repair))
+            {
+                RepairItems(Config.TargetRepairParts, Config.RepairCost);
+            }
+
+            if (!_modIsEnable) {
+                return;
+            }
+
+            if (_runOnes)
+            {
+                GlobalPref();
+            }
+
+            RemoveBlocks();
+
+            GameObject engineRoom = GameObject.Find("EngineRoom");
+            if (engineRoom != null)
+            {
+                var erw = engineRoom.transform.Find("Engine_Room_Workbench_2");
+                if(erw != null)
+                {
+                    var gameobj = erw.gameObject;
+                    gameobj?.SetActive(true);
+                }
+
+                var erbt = engineRoom.transform.Find("Engine_Room_Big_Table");
+                if (erbt != null)
+                {
+                    var gameobj = erbt.gameObject;
+                    gameobj?.SetActive(true);
+                }
+
+            }
 
             GameObject shopCarWindow = GameObject.Find("ShopCarWindow");
             if (shopCarWindow != null)
             {
-                var itemsCars = shopCarWindow.transform.Find("Items");
-                if (itemsCars != null)
+                CheckAndDisableOldCarFromShop(shopCarWindow);
+
+                return;
+            }
+
+            GameObject shopMenu = GameObject.Find("ShopMenu");
+            if (shopMenu != null && _config.IsEnableBrandFilter)
+            {
+                CheckAndDisableBrandsFromShop(shopMenu);
+            }
+        }
+
+        private void RepairItems(string[] repairParts, int[] repairCost)
+        {
+            Inventory inventory = Singleton<Inventory>.Instance;
+            var cItems = inventory.GetItems();
+            foreach (var baseItem in cItems)
+            {
+                if (baseItem.Condition == 1.0f)
                 {
-                    for (int i = 0; i < itemsCars.childCount; i++)
+                    continue;
+                }
+
+                if (Helpers.CheckForSubstrings(baseItem.ID, repairParts))
+                {
+                    var money = GlobalData.PlayerMoney;
+                    var currentCond = baseItem.Condition;
+
+                    if (currentCond < 0.15f && money < repairCost[2])
                     {
-                        Transform ShowroomCarItem21Shop = itemsCars.GetChild(i);
-                        GameObject ShowroomCarObj = ShowroomCarItem21Shop.gameObject;
-                        var carName = ShowroomCarItem21Shop.Find("CarName").GetComponent<Text>();
-                        var carYear = Helpers.GetYearFromString(carName.text);
-                        if (carYear < _config.RemoveCarsOlderThanYear)
-                        {
-                            ShowroomCarObj.SetActive(false);
-                            MelonLogger.Msg($"Car year {carYear}");
-                        }
+                        UIManager.Get().ShowPopup(Config.ModName, "insufficient funds", PopupType.Normal);
+
+                        return;
                     }
 
+                    if (currentCond < 0.4f && money < repairCost[1])
+                    {
+                        UIManager.Get().ShowPopup(Config.ModName, "insufficient funds", PopupType.Normal);
+
+                        return;
+                    }
+
+                    if (currentCond < 0.7f && money < repairCost[0])
+                    {
+                        UIManager.Get().ShowPopup(Config.ModName, "insufficient funds", PopupType.Normal);
+
+                        return;
+                    }
+
+                    float finallyPrice = 0;
+
+                    switch (currentCond)
+                    {
+                        case var cond when cond < 0.15f:
+                            finallyPrice = repairCost[2] + (currentCond * repairCost[2]);
+                            GlobalData.AddPlayerMoney(-(int)finallyPrice);
+                            break;
+
+                        case var cond when cond < 0.4f:
+                            finallyPrice = repairCost[1] + (currentCond * repairCost[1]);
+                            GlobalData.AddPlayerMoney(-(int)finallyPrice);
+                            break;
+
+                        case var cond when cond < 0.7f:
+                            finallyPrice = repairCost[0] + (currentCond * repairCost[0]);
+                            GlobalData.AddPlayerMoney(-(int)finallyPrice);
+                            break;
+                    }
+
+                    baseItem.Condition = 1.0f;
+                    baseItem.Dent = 1.0f;
+
+                    UIManager.Get().ShowPopup(Config.ModName, $"item was repair! cost: {finallyPrice}", PopupType.Normal);
+                } else
+                {
+                    MelonLogger.Msg($"{Helpers.ToPrettyString(baseItem)}");
                 }
             }
         }
+
+        private void CheckAndDisableOldCarFromShop(GameObject shopCarWindow)
+        {
+
+            var itemsCars = shopCarWindow.transform.Find("Items");
+            if (itemsCars != null)
+            {
+
+                for (int i = 0; i < itemsCars.childCount; i++)
+                {
+                    Transform ShowroomCarItem21Shop = itemsCars.GetChild(i);
+                    var carName = ShowroomCarItem21Shop.Find("CarName").GetComponent<Text>();
+                    var carYear = Helpers.GetYearFromString(carName.text);
+
+                    if (carYear < _config.RemoveCarsOlderThanYear)
+                    {
+                        GameObject ShowroomCarObj = ShowroomCarItem21Shop.gameObject;
+                        ShowroomCarObj.SetActive(false);
+
+                        if (_config.IsShowDebugInfo)
+                        {
+                            MelonLogger.Msg($"car year {carYear}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CheckAndDisableBrandsFromShop(GameObject shopMenu)
+        {
+
+/*            var mainLableText = shopMenu.transform.Find("MainLabel").GetComponent<Text>();
+            if (mainLableText != null) {
+                MelonLogger.Msg($"main label");
+            }*/
+
+            var items = shopMenu.transform.Find("Items");
+            if (items != null)
+            {
+                for (int i = 0; i < items.childCount; i++)
+                {
+                    Transform part = items.GetChild(i);
+                    if (part == null)
+                    {
+                        continue;
+                    }
+
+                    var partBrand = part.Find("Brand");
+                    if (partBrand != null)
+                    {
+                        var image = partBrand.GetComponent<Image>();
+                        string imageName = image.sprite.name;
+
+                        if (!Helpers.IsBrandExist(imageName))
+                        {
+                            GameObject ShowroomCarObj = part.gameObject;
+                            ShowroomCarObj.SetActive(false);
+                        }
+
+                        //if (Input.GetKeyDown(_config.PrintStack))
+                       // {
+                        //MelonLogger.Msg($"id {i} texture {imageName}");
+                       // }
+                    }
+                }
+            }
+        }
+
+        private void RemoveBlocks()
+        {
+            var engineRoomBlocker = GameObject.Find("EngineRoomBlocker");
+            engineRoomBlocker?.SetActive(false);
+
+            _blocker_Repair_Parts_2 = GameObject.Find("Blocker_Repair_Parts_2");
+            _blocker_Repair_Parts_2?.SetActive(false);
+
+            _blocker_Repair_Parts_3 = GameObject.Find("Blocker_Repair_Parts_3");
+            _blocker_Repair_Parts_3?.SetActive(false);
+        }
+
+        private void GlobalPref()
+        {
+            _runOnes = false;
+
+            var itemsMain = Singleton<GameInventory>.Instance.GetItems(ShopType.Main);
+            SetAllRepairs(itemsMain);
+
+            var itemsBody = Singleton<GameInventory>.Instance.GetItems(ShopType.Body);
+            foreach (var item in itemsBody)
+            {
+                item.RepairGroup = 6;
+            }
+
+            var itemsInterior = Singleton<GameInventory>.Instance.GetItems(ShopType.Interior);
+            foreach (var item in itemsInterior)
+            {
+                item.RepairGroup = 6;
+            }
+
+            var itemsTire = Singleton<GameInventory>.Instance.GetItems(ShopType.Tire);
+            SetAllRepairs(itemsTire);
+            var itemsLicensePlate = Singleton<GameInventory>.Instance.GetItems(ShopType.LicensePlate);
+            SetAllRepairs(itemsLicensePlate);
+            var itemsTuning = Singleton<GameInventory>.Instance.GetItems(ShopType.Tuning);
+            SetAllRepairs(itemsTuning);
+            var itemsBodyTuning = Singleton<GameInventory>.Instance.GetItems(ShopType.BodyTuning);
+            SetAllRepairs(itemsBodyTuning);
+            var itemsRims = Singleton<GameInventory>.Instance.GetItems(ShopType.Rims);
+            SetAllRepairs(itemsRims);
+            var itemsGearbox = Singleton<GameInventory>.Instance.GetItems(ShopType.Gearbox);
+            SetAllRepairs(itemsGearbox);
+            var itemsElectronics = Singleton<GameInventory>.Instance.GetItems(ShopType.Electronics);
+            SetAllRepairs(itemsElectronics);
+            var itemsAddons = Singleton<GameInventory>.Instance.GetItems(ShopType.Addons);
+            SetAllRepairs(itemsAddons);
+        }
+
+        private void SetAllRepairs(Il2CppSystem.Collections.Generic.List<PartProperty> items)
+        {
+            MelonLogger.Msg($"start set repair groups {items.Count}");
+            foreach (var item in items)
+            {
+                if (!Helpers.IsBrandExist(item.Brand))
+                {
+                    item.RepairGroup = 6;
+                }
+            }
+
+            MelonLogger.Msg($"finished repair groups");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            LoggerInstance.Msg($"Scene {sceneName} with build index {buildIndex} has been loaded!");
+            if (sceneName == "garage" && buildIndex == 10)
+            {
+                _runOnes = true;
+
+                MelonLogger.Msg($"GlobalData.Cost_TravelJunkyard = {GlobalData.Cost_TravelJunkyard} ");
+            }
+        }
+
     }
 }
 
